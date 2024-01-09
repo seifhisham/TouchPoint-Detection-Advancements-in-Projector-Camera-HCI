6abaf4bb8833add74c495a2ebccde28ddd818a70
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2024-01-09 20:52:42 +0200

    organized the frontend code and separated it from the backend

--- Virtual_Mouse.py
+++ Virtual_Mouse.py
@@ -3,51 +3,29 @@ import numpy as np
 import time
 import HandTracking as ht
 import autopy
-import tkinter as tk
-from threading import Thread
 
-
-class HandTrackingApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Hand Tracking App")
-
-        self.start_button = tk.Button(root, text="Start", command=self.start_hand_tracking)
-        self.start_button.pack(pady=10)
-
-        self.stop_button = tk.Button(root, text="Stop", command=self.stop_hand_tracking)
-        self.stop_button.pack(pady=10)
-
+class VirtualMouse:
+    def __init__(self, app):
+        self.app = app
         self.cap = cv2.VideoCapture(0)
         self.cap.set(3, 640)
         self.cap.set(4, 480)
 
         self.detector = ht.handDetector(maxHands=1)
 
-        self.is_tracking = False
-
-    def start_hand_tracking(self):
-        if not self.is_tracking:
-            self.is_tracking = True
-            Thread(target=self.hand_tracking_loop).start()
-
-    def stop_hand_tracking(self):
-        self.is_tracking = False
-        self.root.destroy()
-
     def hand_tracking_loop(self):
-        pTime = 0 # Used to calculate frame rate
-        width, height = 640, 480 # Width and height of Camera
-        frameR = 100 # Frame Rate
-        smoothening = 8 # Smoothening Factor
-        prev_x, prev_y = 0, 0 # Previous coordinates
-        curr_x, curr_y = 0, 0 # Current coordinates
-        roi_radius = 50 # Adjust the radius based on your preference
-        roi_center = (width // 2, height // 2) # Initial center of the ROI
+        pTime = 0
+        width, height = 640, 480
+        frameR = 100
+        smoothening = 8
+        prev_x, prev_y = 0, 0
+        curr_x, curr_y = 0, 0
+        roi_radius = 50
+        roi_center = (width // 2, height // 2)
 
         screen_width, screen_height = autopy.screen.size()
 
-        while self.is_tracking:
+        while self.app.is_tracking:
             success, img = self.cap.read()
             img = self.detector.findHands(img)
             lmlist, bbox = self.detector.findPosition(img)
@@ -89,9 +67,3 @@ class HandTrackingApp:
             cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
             cv2.imshow("Image", img)
             cv2.waitKey(1)
-
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = HandTrackingApp(root)
-    root.mainloop()
